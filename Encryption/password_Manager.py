37ad259d3d957a8f1afe680684ab5dcb3f1bdd9e
justus-fischer <266495557+Justus-Fischer@users.noreply.github.com>
2026-07-19 14:44:38 +0200

    basics (cloned from Encryption_Tool.py)

--- Encryption/password_Manager.py
+++ Encryption/password_Manager.py
@@ -1,2 +1,80 @@
 #just a demo 
 #Do not use for real passwords 
+
+import random
+import secrets
+
+
+def bu(num):
+    word = []
+    for i in range(num):
+        sp = chr(random.randint(0, 130))
+        while not sp.isprintable():
+            sp = chr(random.randint(0, 130))
+    
+        word.append(sp)
+    return "".join(word)
+
+def zkv(wert):
+    wert = wert % 0x110000  
+    if 0xD800 <= wert <= 0xDFFF:  
+        wert = wert + 2048  
+    return wert
+
+def zke(wert):
+    wert = wert % 0x110000  
+    if 0xD800 <= wert <= 0xDFFF:  
+        wert = wert - 2048  
+    return wert % 0x110000
+
+def seedgen(pasw):
+    stes = 5381
+    
+    bpasw = list(pasw)
+    
+    for i in range(len(bpasw)):
+        stes = int((stes * 33) + ord(bpasw[i]))
+        
+    return stes % (2**64)
+
+def crypto(mes,mode):
+    
+    if mode == 1:
+        bmes = list(mes) 
+        random.seed(seedgen(key)+iv)
+        for i in range (len(bmes)):
+            bmes[i] = ord(bmes[i])
+            try:
+                if i > 0:
+                    bmes[i] = chr(zkv(bmes[i] + random.randint(100, 100000) - ord(bmes[i-1])*10))
+                    
+                else:
+                    bmes[i] = chr(zkv(bmes[i] + random.randint(100, 100000)*iv))
+                    
+            except:
+                print("unexpected error")
+                break
+    else:
+        ivT = mes[:6]
+        re = geheimtext = mes[6:]
+        bmes = list(re)
+        cbmes = list(re)
+        random.seed(seedgen(key)+int(ivT))
+        for i in range (len(bmes)):
+            bmes[i] = ord(bmes[i])
+            try:
+                if i > 0:
+                    bmes[i] = chr(zke(bmes[i] - random.randint(100, 100000) + ord(cbmes[i-1])*10))
+                    
+                else:
+                    bmes[i] = chr(zke(bmes[i] - random.randint(100, 100000)*int(ivT)))
+                    
+            except:
+                print("invalid password")
+                break
+    try:
+        return "".join(bmes)
+    except:
+        print("Please try again")
+    
+            
